c852c55ee6069eecb51e7f4223f5b11e3c70a052
jeswi2 <jeswimathewm@gmail.com>
2022-10-10 22:06:37 +0530

    ro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { PostlistComponent } from './postlist/postlist.component';
+
+const appRoutes:Routes=[
+  {
+    path:"",component:PostlistComponent
+  }
+]
 
 @NgModule({
   declarations: [
@@ -14,7 +21,8 @@ import { PostlistComponent } from './postlist/postlist.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
